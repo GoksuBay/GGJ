428f0b1f089c1c5940479c796f7fb59516e79b18
GoksuBay <46191786+GoksuBay@users.noreply.github.com>
2020-02-01 10:09:43 +0300

    10.09

--- TestGGJ/Node2D.gd
+++ TestGGJ/Node2D.gd
@@ -15,6 +15,9 @@ var time
 var temp_s = 0
 var earthrot
 var rubbish
+var G = 255
+var B = 255
+var yuzde
 
 
 # Declare member variables here. Examples:
@@ -30,22 +33,22 @@ func _ready():
 	numofTrees = 10000
 	numofSeeds = 0
 	rubbish = 0
+	get_node("Sprite").modulate = Color8(255 , 0 , 0)
 	pass # Replace with function body.
-	
-	
+
 
 func temp_event(numofPeople):
-	if numofTrees < 5000:
+	if temperature > 21 && numofTrees < 5000:
 		temperature = 26-(plantedSeeds/10)
-	if numofTrees < 1000:
+	if temperature > 21 && numofTrees < 1000:
 		temperature = 33-(plantedSeeds/10)
-	if numofTrees < 500:
+	if temperature > 21 && numofTrees < 500:
 		temperature = 40-(plantedSeeds/10)
-	if numofTrees < 250:
+	if temperature > 21 && numofTrees < 250:
 		temperature = 46-(plantedSeeds/10)
-	if numofTrees < 150:
+	if temperature > 21 && numofTrees < 150:
 		temperature = 52-(plantedSeeds/10)
-	if numofTrees < 50:
+	if temperature > 21 && numofTrees < 50:
 		temperature = 63
 		pass
 
@@ -55,12 +58,16 @@ func _physics_process(delta):
 	time = s
 	if game_ending() != true:
 		during_game()
+	if numofTrees != 10000 && numofTrees%100 == 0:
+		G -= 1
+		B -= 1
+	get_node("Sprite").modulate = Color8(255 , G , B)
 	if temp_s*numofPeople > 10000 && game_ending() != true:
 		warm()
 		temp_s = 0
-	
+		
 	var TreeLabel = get_node("Trees")
-	TreeLabel.text = str(numofTrees)
+	TreeLabel.text = str("%" , float(100*numofTrees/10000))
 	
 	var SeedLabel = get_node("Seeds")
 	SeedLabel.text = str(numofSeeds)
@@ -74,6 +81,11 @@ func _physics_process(delta):
 	
 	var RubbishLabel = get_node("Rubbish")
 	RubbishLabel.text = str(rubbish)
+	
+	var PlantedLabel = get_node("Planted")
+	PlantedLabel.text = str(plantedSeeds)
+	
+	visibility()
 	
 	if game_ending():
 		get_node("Game Over").visible = true
@@ -132,3 +144,8 @@ func collect():
 	if rubbish != 0:
 		rubbish -= 1
 	pass
+
+func visibility():
+	if numofTrees < 9900:
+		get_node("Earth/tree2").visible = false
+	
